6753e73a7b4e07e2a960c8b3a7d8091d9cf9dfdb
Daniel Souto Rodrigues <dantdmc210@gmail.com>
2024-12-02 17:58:00 -0300

    css em rotas, parametros, e escutando mudancas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursosDetalheComponent } from './cursos/cursos-detalhe/cursos-detalhe.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent},
   { path: 'cursos', component: CursosComponent},
+  { path: 'curso/:id', component: CursosDetalheComponent}
 ];
 
 @NgModule({
--- src/app/cursos/cursos-detalhe/cursos-detalhe.component.ts
+++ src/app/cursos/cursos-detalhe/cursos-detalhe.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cursos-detalhe',
-  template: `<p>cursos-detalhe works!</p>`
+  templateUrl: './cursos-detalhe-component.html'
 })
 export class CursosDetalheComponent implements OnInit {
 
-  constructor() { }
+  id!: string;
+  inscricao!: Subscription;
+
+  constructor(private route: ActivatedRoute) { 
+    // this.id = this.route.snapshot.params['id'];
+    // console.log(this.id);
+  }
 
   ngOnInit(): void {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
   }
 
 }
